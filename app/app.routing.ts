ddae1319c1e98ffac39ba4f8cba9ddc49b611529
Jack Zhang <jzhanglsw@gmail.com>
2016-12-26 13:38:50 -0500

    Contact component

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { SkillsComponent } from './sidebarComponents/skillsComponent';
 import { HomeComponent } from './sidebarComponents/homeComponent';
+import { SkillsComponent } from './sidebarComponents/skillsComponent';
+import { ContactComponent } from './sidebarComponents/contactComponent';
 import { PageNotFoundComponent } from './sidebarComponents/pageNotFoundComponent';
+
 
 const routes: Routes = [
 	{ path: '',  redirectTo: '/home', pathMatch: 'full'},
 	{ path: 'skills', component: SkillsComponent },
 	{ path: 'home', component: HomeComponent },
+	{ path: 'contact', component: ContactComponent },
 	{ path: '**', component: PageNotFoundComponent }
 ];
 
@@ -17,4 +20,4 @@ const routes: Routes = [
 })
 export class AppRoutingModule {}
 
-export const routingComponents = [SkillsComponent, HomeComponent, PageNotFoundComponent];
+export const routingComponents = [SkillsComponent, HomeComponent, PageNotFoundComponent, ContactComponent];
---
+++ app/sidebarComponents/contactComponent.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: 'app/template/contact.component.html',
+  styleUrls: ['app/css/main.css']
+})
+export class ContactComponent  {}
